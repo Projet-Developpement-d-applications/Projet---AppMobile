40fe4aebeeddba0126b8b9b06ebb74acd72dd700
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-06-17 18:33:16 -0400

    joueurs fini

--- conquerantsMobile/screens/Joueurs.jsx
+++ conquerantsMobile/screens/Joueurs.jsx
@@ -11,6 +11,7 @@ import { useJeu } from '../Context/JeuContext';
 import JoueurImage from '../images/joueur.jpg';
 import { ScrollView } from 'react-native-gesture-handler';
 import { getImage } from '../components/JoueurImage';
+import lang from '../json/lang.json';
 
 function Joueurs() {
     const { langue } = useLangue();
@@ -18,10 +19,13 @@ function Joueurs() {
     const { equipes } = useEquipe();
     const { jeux } = useJeu();
 
+    const [stats, setStats] = useState([]);
+    const [statsFr, setStatsFr] = useState([]);
     const [joueurs, setJoueurs] = useState([]);
     const [loading, setLoading] = useState(true);
     const [erreur, setErreur] = useState(false);
     const [image, setImage] = useState();
+    const [moyenneStats, setMoyenneStats] = useState("");
 
     const [equipeFiltre, setEquipeFiltre] = useState({ nom: "" });
     const [jeuFiltre, setJeuFiltre] = useState({ nom: "" });
@@ -46,6 +50,100 @@ function Joueurs() {
 
         fetchData();
     }, []);
+
+    useEffect(() => {
+        switch(joueurFiltre.jeu) {
+            case "Valorant":
+                setStats(langue.valo.statsMoyenne);
+                setStatsFr(lang.fr.valo.stats);
+                break;
+
+            case "League of Legends":
+                setStats(langue.lol.statsMoyenne);
+                setStatsFr(lang.fr.lol.stats);
+                break;
+
+            case "Rocket League":
+                setStats(langue.rl.statsMoyenne);
+                setStatsFr(lang.fr.rl.stats);
+                break;
+            default :
+                break;
+        }
+    }, [joueurFiltre, langue]);
+
+    useEffect(() => {
+        const plusFrequent = (array) => {
+            const occurrences = {};
+    
+            array.forEach((value) => {
+                occurrences[value] = (occurrences[value] || 0) + 1;
+            });
+    
+            let maxOccurrence = 0;
+            let mostFrequentValue;
+            for (const [value, occurrence] of Object.entries(occurrences)) {
+                if (occurrence > maxOccurrence) {
+                    maxOccurrence = occurrence;
+                    mostFrequentValue = value;
+                }
+            }
+    
+            return mostFrequentValue;
+        };
+    
+        const getMoyenne = (data) => {
+            const length = stats.length;
+            const total = Array.from({ length }, () => 0);
+            const nbrStat = Array.from({ length }, () => 0);
+            const moyenne = Array.from({ length }, () => 0);
+        
+            data.forEach((stat) => {
+                var statJson = JSON.parse(stat.donnee);
+        
+                statsFr.forEach((statFr, index) => {
+                    if (statJson[statFr] && statJson[statFr] !== "0") {
+        
+                        var numValue = parseFloat(statJson[statFr]);
+        
+                        nbrStat[index]++;
+        
+                        if (!isNaN(numValue)) {
+                            total[index] += numValue;
+                        } else {
+                            if (statFr === lang.fr.victoire) {
+                                total[index] += statJson[statFr] === "Victoire" ? 100 : 0;
+                            } else {
+                                if (Array.isArray(total[index])) {
+                                    total[index].push(statJson[statFr]);
+                                } else {
+                                    total[index] = [statJson[statFr]];
+                                }
+                            }
+                        }
+                    }
+                });
+            });
+        
+            for (var i = 0; i < length; i++) {
+                if (!Array.isArray(total[i])) {
+                    moyenne[i] = Math.round((total[i] / nbrStat[i]) * 100) / 100;
+                } else {
+                    moyenne[i] = plusFrequent(total[i]);
+                }
+            }
+        
+            if (moyenne[0]) {
+                setMoyenneStats(moyenne);
+            } else {
+                setMoyenneStats("");
+            }
+        };
+    
+        if (joueurFiltre.nom !== "") {
+            getMoyenne(joueurFiltre.statistiques);
+        }
+    }, [joueurFiltre, stats, statsFr]);
 
     useEffect(() => {
         const getImageJoueur = () => {
@@ -120,6 +218,21 @@ function Joueurs() {
                         <Text style={styles.joueurAutreTexte}>{joueurFiltre.equipe.nom}</Text>
                         <Text style={styles.joueurAutreTexte}>{joueurFiltre.position}</Text>
                     </View>
+                </View>
+                <View style={styles.joueurStats}>
+                    <Text style={styles.statsTitre}>{langue.joueurs.statsMoyenne}</Text>
+                    {moyenneStats[0] ? (
+                        <View style={styles.moyenne}>
+                            {moyenneStats.map((stat, index) => (
+                                <View style={styles.statsCombo} key={index}>
+                                    <Text style={styles.statsHeader}>{stats[index]}</Text>
+                                    <Text style={styles.statsTexte}>{stat}</Text>
+                                </View>
+                            ))}
+                        </View>
+                    ) : (
+                        <Text style={styles.statsTitre}>{langue.joueurs.aucune}</Text>
+                    )}
                 </View>
             </View>}
         </ScrollView>
@@ -256,7 +369,8 @@ const styles = StyleSheet.create({
         width: '100%',
         flexDirection: 'row',
         backgroundColor: '#000',
-        borderRadius: 15,
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
     },
     joueurTexte: {
         padding: 20,
@@ -275,6 +389,46 @@ const styles = StyleSheet.create({
     },
     joueurNom: {
         marginBottom: 0,
+    },
+    fillJoueur: {
+        marginBottom: 20,
+    },
+    statsTitre: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    joueurP: {
+        fontSize: 16,
+    },
+    joueurStats: {
+        marginBottom: 20,
+        backgroundColor: '#000',
+        padding: 10,
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+    },
+    statsTexte: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    statsCombo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 8,
+    },
+    statsHeader: {
+        flex: 1,
+        fontWeight: 'bold',
+        flexWrap: 'wrap',
+        color: '#fff',
+        fontSize: 16,
+    },
+    moyenne: {
+        flexDirection: 'column',
     },
 });
 
