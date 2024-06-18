62abe226e176faa06584397b679e311f2e2e93e9
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-06-18 19:00:51 -0400

    removed web

--- conquerants/src/components/Encryption.jsx
+++
@@ -1,9 +0,0 @@
-import JSEncrypt from 'jsencrypt';
-import { publicKey } from '../publicKey';
-
-export function encrypt(data) {
-  const encrypt = new JSEncrypt();
-  encrypt.setPublicKey(publicKey);
-  const encrypted = encrypt.encrypt(data);
-  return encrypted;
-}
